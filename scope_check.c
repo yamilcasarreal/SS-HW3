31f57225bf408adc1436c4b7592b03a2c19804ca
rschroeder <trebor.clef@gmail.com>
2023-11-05 14:08:45 -0500

    added my code from scope check

--- scope_check.c
+++ scope_check.c
@@ -292,3 +292,264 @@ id_use *scope_check_ident_declared(
     }
     return ret;
 }
+
+/*
+
+void scope_check_program(block_t prog)
+{
+   scope_check_block(prog);
+}
+
+void scope_check_block(block_t blk)
+{  
+   symtab_enter_scope();
+   scope_check_constDecls(blk.const_decls);
+   scope_check_varDecls( blk.var_decls);
+   scope_check_procDecls(blk.proc_decls);
+   scope_check_stmt( blk.stmt);
+   symtab_leave_scope();
+
+}
+
+void scope_check_constDecls(const_decls_t cds)
+{
+   const_decl_t *cd_listp = cds.const_decls;
+   while (cd_listp != NULL)
+   {
+      scope_check_constDecl(*cd_listp);
+      cd_listp = cd_listp->next;
+   }
+}
+
+void scope_check_constDecl(const_decl_t cd)
+{
+   scope_check_constDefs(cd.const_defs);
+}
+
+void scope_check_constDefs(const_defs_t cdfs)
+{
+   const_def_t *cdp = cdfs.const_defs;
+   while (cdp != NULL)
+   {
+      scope_check_constDef(*cdp);
+      cdp = cdp->next;
+   }
+
+}
+//TODO:  
+void scope_check_constDef(const_defs_t cdf)
+{
+
+}
+
+void scope_check_varDecls(var_decls_t vds)
+{
+   var_decl_t *vdp = vds.var_decls;
+   while (vdp != NULL)
+   {
+      scope_check_varDecl(*vdp);
+      vdp = vdp->next;
+   }
+}
+
+void scope_check_varDecl(var_decl_t vd)
+{
+   scope_check_idents( vd.idents);
+}
+
+//TODO: 
+void scope_check_idents(idents_t idents)
+{
+   ident_t *ip = idents.idents;
+   while (ip != NULL)
+   {
+
+   }
+}
+
+void scope_check_procDecls(proc_decls_t pds)
+{
+   proc_decl_t *pdp = pds.proc_decls;
+   while (pdp != NULL)
+   {
+      scope_check_procDecl(*pdp);
+      pdp = pdp ->next;
+   }
+
+}
+
+void scope_check_procDecl(proc_decl_t pd)
+{
+   scope_check_block(*(pd.block));
+
+}
+
+void scope_check_stmt(stmt_t stmt)
+{
+   switch (stmt.stmt_kind) 
+   {
+    case assign_stmt:
+      scope_check_assignStmt(stmt.data.assign_stmt);
+      break;
+    case call_stmt:
+      scope_check_callStmt(stmt.data.call_stmt);
+      break;
+    case begin_stmt:
+      scope_check_beginStmt(stmt.data.begin_stmt);
+      break;
+    case if_stmt:
+      scope_check_ifStmt(stmt.data.if_stmt);
+      break;
+    case while_stmt:
+      scope_check_whileStmt(stmt.data.while_stmt);
+      break;
+    case read_stmt:
+      scope_check_readStmt(stmt.data.read_stmt);
+      break;
+    case write_stmt:
+      scope_check_writeStmt(stmt.data.write_stmt);
+      break;
+    case skip_stmt:
+      scope_check_skipStmt();
+      break;
+    default:
+      bail_with_error("Unknown stmt_kind (%d) in scope_check_stmt!",
+			stmt.stmt_kind);
+	   break;
+
+   }
+}
+
+void scope_check_assignStmt( assign_stmt_t stmt)
+{
+   if (stmt.expr == NULL) 
+   {
+	bail_with_error("Found null expression in assignment statment!");
+   }
+
+   scope_check_expr(*(stmt.expr));
+   
+}
+
+//TODO:
+void scope_check_callStmt(call_stmt_t stmt)
+{
+}
+
+void scope_check_beginStmt(begin_stmt_t stmt)
+{
+   scope_check_stmts(stmt.stmts);
+}
+void scope_check_stmts( stmts_t stmts)
+{
+   stmt_t * sp = stmts.stmts;
+   while (sp != NULL)
+   {
+      scope_check_stmt(*sp);
+      sp = sp->next;
+   }
+}
+void scope_check_ifStmt(if_stmt_t stmt)
+{
+   scope_check_condition(stmt.condition);
+   scope_check_stmt(*(stmt.then_stmt));
+   scope_check_stmt(*(stmt.else_stmt));
+}
+void scope_check_whileStmt(while_stmt_t stmt)
+{
+   scope_check_condition(stmt.condition);
+   scope_check_stmt(*(stmt.body));
+}
+//TODO:
+void scope_check_readStmt(read_stmt_t stmt)
+{
+
+}
+void scope_check_writeStmt(write_stmt_t stmt)
+{
+   scope_check_expr(stmt.expr);
+}
+//TODO:
+void scope_check_skipStmt()
+{
+}
+
+void scope_check_condition(condition_t cond)
+{
+   switch (cond.cond_kind) 
+   {
+      case ck_odd:
+         scope_check_oddCond(cond.data.odd_cond);
+         break;
+      case ck_rel:
+         scope_check_relOpCond(cond.data.rel_op_cond);
+         break;
+      default:
+	bail_with_error("Unexpected condition_kind_e (%d) in unparseCondition!",
+			cond.cond_kind);
+	break;
+      
+   
+   }
+}
+
+void scope_check_oddCond(odd_condition_t cond)
+{
+   scope_check_expr(cond.expr);
+}
+
+void scope_check_relOpCond(rel_op_condition_t cond)
+{
+   scope_check_expr(cond.expr1);
+   scope_check_token(cond.rel_op);
+   scope_check_expr(cond.expr2);
+
+}
+
+//TODO:
+void scope_check_token()
+{
+
+}
+
+void scope_check_expr(expr_t exp)
+{
+   switch (exp.expr_kind) 
+   {
+      case expr_bin:
+         scope_check_binOpExpr(exp.data.binary);
+         break;
+      case expr_ident:
+         scope_check_ident(exp.data.ident);
+         break;
+      case expr_number:
+         scope_check_number(exp.data.number);
+	      break;
+      default:
+	bail_with_error("Unexpected expr_kind_e (%d) in scope_check_expr!",
+			exp.expr_kind);
+	      break;
+
+   }
+
+}
+
+void scope_check_binOpExpr(binary_op_expr_t exp)
+{
+   scope_check_expr(*(exp.expr1));
+   scope_check_token(exp.arith_op);
+    scope_check_expr(*(exp.expr2));
+}
+//TODO:
+void scope_check_ident(ident_t id)
+{
+
+}
+
+//TODO:
+void scope_check_number(number_t num)
+{
+
+}
+
+*/
